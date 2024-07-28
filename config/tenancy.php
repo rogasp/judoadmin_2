b2b037ec5edd4b889e51e3a432d537a03ff5baad
Roger Aspelin <roger.aspelin@hotmail.se>
2024-07-28 02:05:44 +0200

    Initial 2

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,9 +17,7 @@ return [
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains' => [
-        'judoadmin.local',
-    ],
+    'central_domains' => env('TENANCY_CENTRAL_DOMAINS', '[]'),
 
     /**
      * Tenancy bootstrappers are executed when tenancy is initialized.
