db17cf9989ab2e42d8447cf54c28dabbca760863
Roger Aspelin <roger.aspelin@hotmail.se>
2024-07-28 02:10:04 +0200

    Initial 2

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,7 +17,7 @@ return [
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains' => env('TENANCY_CENTRAL_DOMAINS', '[]'),
+    'central_domains' => explode(',', env('TENANCY_CENTRAL_DOMAINS', '')),
 
     /**
      * Tenancy bootstrappers are executed when tenancy is initialized.
